d08e7ec3ed0c0ba5cfd730757b3e59afd2f49a75
TROLLGAMES <73804955+yTROLLGAMES@users.noreply.github.com>
2021-06-26 15:26:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+var app = require('http').createServer(resposta);
+app.listen(3000);
+console.log("Aplicação está em execução...");
+function resposta (req, res) {
+     res.writeHead(200);
+     res.end("Ola, o servidor esta funcionando corretamente.");
+}
